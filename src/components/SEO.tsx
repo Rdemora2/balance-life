117902ac4b7b4932711cf38213b7e1fa6f2d8742
Rdemora2 <robertomoraeszar@gmail.com>
2025-03-30 13:58:13 -0300

    FEAT/ Adiciona componente SEO para meta tags e compartilhamento social

---
+++ src/components/SEO.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+interface SEOProps {
+  title: string;
+  description: string;
+  image?: string;
+  url?: string;
+  type?: string;
+}
+
+const SEO: React.FC<SEOProps> = ({
+  title,
+  description,
+  image = "https://balancelife.com.br/og-image.jpg",
+  url = "https://balancelife.com.br",
+  type = "website",
+}) => {
+  const siteTitle = "Balance Life";
+  const fullTitle = `${title} | ${siteTitle}`;
+
+  return (
+    <Helmet>
+      {/* Meta tags básicas */}
+      <title>{fullTitle}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content={type} />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={fullTitle} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={fullTitle} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
+
+      {/* Favicon */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    </Helmet>
+  );
+};
+
+export default SEO;
